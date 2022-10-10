8c84e02ffd898f9d02ee16f82bb29e1af4093524
chaitanya-191855 <raavichaitu4014@gmail.com>
2022-10-10 15:41:11 +0530

    intial

---
+++ main.js
@@ -0,0 +1,11 @@
+let var1 = document.getElementById("output");
+
+function add(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+const mul = (a, b) => a * b;
+var1.innerHTML = sub(2, 3);
+ 
